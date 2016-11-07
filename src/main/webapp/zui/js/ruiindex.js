5896911ec86612516768f36a1b952c8f34e58d43
nilszhang@gmail.com <nilszhang@gmail.com>
2016-11-07 15:09:47 +0800

    基本框架

--- src/main/webapp/zui/js/ruiindex.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by nilszhang on 2016/2/2.
- */
-
-function addTab()
-{
-    alert('tab');
-    $("#tab2").tab('show');
-}
--- src/main/webapp/zui/lib/chart/zui.chart.js
+++ src/main/webapp/zui/lib/chart/zui.chart.js
@@ -1,11 +1,28 @@
 /*!
- * Chart.js
+ * ZUI: Chart.js - v1.5.0 - 2016-09-06
+ * http://zui.sexy
+ * GitHub: https://github.com/easysoft/zui.git 
+ * Copyright (c) 2016 cnezsoft.com; Licensed MIT
+ */
+
+/* ========================================================================
+ * Chart.js: Chart.Core.js [Version: 1.0.2]
  * http://chartjs.org/
- * Version: 1.0.2
- *
+ * 
+ * ZUI: The file has been changed in ZUI. It will not keep update with the
+ * official version in the future.
+ * http://zui.sexy
+ * ========================================================================
+ * Copyright 2015 Nick Downie, Released under the MIT license
+ * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
+ * ======================================================================== */
+
+
+/*!
+ * Chart.js 1.0.2 
  * Copyright 2015 Nick Downie
  * Released under the MIT license
- * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
+ * http://chartjs.org/
  */
 
 /// ----- ZUI change begin -----
@@ -2055,15 +2072,18 @@
 /// ----- ZUI change end -----
 
 
-/*!
- * Chart.js
+/* ========================================================================
+ * Chart.js: Chart.line.js [Version: 1.0.2]
  * http://chartjs.org/
- * Version: 1.0.2
- *
- * Copyright 2015 Nick Downie
- * Released under the MIT license
+ * 
+ * ZUI: The file has been changed in ZUI. It will not keep update with the
+ * official version in the future.
+ * http://zui.sexy
+ * ========================================================================
+ * Copyright 2015 Nick Downie, Released under the MIT license
  * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
- */
+ * ======================================================================== */
+
 
 /// ----- ZUI change begin -----
 /// Add jquery object to namespace
@@ -2492,15 +2512,17 @@
 /// ----- ZUI change end -----
 
 
-/*!
- * Chart.js
+/* ========================================================================
+ * Chart.js: Chart.Doughnut.js [Version: 1.0.2]
  * http://chartjs.org/
- * Version: 1.0.2
- *
- * Copyright 2015 Nick Downie
- * Released under the MIT license
+ * 
+ * ZUI: The file has been changed in ZUI. It will not keep update with the
+ * official version in the future.
+ * http://zui.sexy
+ * ========================================================================
+ * Copyright 2015 Nick Downie, Released under the MIT license
  * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
- */
+ * ======================================================================== */
 
 /// ----- ZUI change begin -----
 /// Add jquery object to namespace
@@ -2626,7 +2648,7 @@
                 segment.color = color.toCssStr();
                 if(!segment.highlight) segment.highlight = color.lighten(5).toCssStr();
             }
-            /// ----- ZUI change begin -----
+            /// ----- ZUI change end -----
             var index = atIndex || this.segments.length;
             this.segments.splice(index, 0, new this.SegmentArc({
                 id: typeof segment.id === 'undefined' ? index : segment.id,
@@ -2642,7 +2664,7 @@
                 circumference: (this.options.animateRotate) ? 0 : this.calculateCircumference(segment.value),
                 /// ----- ZUI change begin -----
                 showLabel: segment.showLabel !== false,
-                /// ----- ZUI change begin -----
+                /// ----- ZUI change end -----
                 label: segment.label
             }));
             if(!silent) {
@@ -2722,7 +2744,7 @@
             var chartWidthHalf = this.chart.width / 2;
             var chartHeightHalf = this.chart.height / 2;
 
-            if(placement === 'outside') {
+            if(placement === 'outside') { // outside
                 var isRight = x >= 0;
                 var lineX = x + chartWidthHalf;
                 var lineY = y + chartHeightHalf;
@@ -2742,7 +2764,8 @@
                     else labelPos++;
                 }
                 while(labelPosMap[isRight ? labelPos : (-labelPos)] && labelPos < maxPos) labelPos++;
-                if(labelPosMap[labelPos]) return;
+
+                if(labelPosMap[isRight ? labelPos : (-labelPos)]) return;
                 y = (labelPos - 1) * textHeight + options.scaleFontSize / 2;
                 labelPosMap[labelPos] = true;
 
@@ -2755,12 +2778,11 @@
                 ctx.strokeWidth = options.scaleLineWidth;
                 ctx.stroke();
                 ctx.fillStyle = segment.fillColor;
-            } else { // outside
+            } else { // inside
                 x = x * 0.6 + chartWidthHalf;
                 y = y * 0.6 + chartHeightHalf;
                 ctx.fillStyle = ($.zui && $.zui.Color) ? (new $.zui.Color(segment.fillColor).contrast().toCssStr()) : '#fff';
             }
-
             ctx.fillText(text, x, y);
         },
         // ZUI change end
@@ -2836,15 +2858,18 @@
 /// ----- ZUI change end -----
 
 
-/*!
- * Chart.js
+/* ========================================================================
+ * Chart.js: Chart.Bar.js [Version: 1.0.2]
  * http://chartjs.org/
- * Version: 1.0.2
- *
- * Copyright 2015 Nick Downie
- * Released under the MIT license
+ * 
+ * ZUI: The file has been changed in ZUI. It will not keep update with the
+ * official version in the future.
+ * http://zui.sexy
+ * ========================================================================
+ * Copyright 2015 Nick Downie, Released under the MIT license
  * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
- */
+ * ======================================================================== */
+
 
 /// ----- ZUI change begin -----
 /// Add jquery object to namespace
